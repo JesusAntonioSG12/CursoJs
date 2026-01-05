e3c1b8f948429a960a5686c90b1fc869d1414180
JesusAntonioSG12 <jesusantonio1506@gmail.com>
2026-01-05 17:08:00 -0600

    Distintos tipos de funciones

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -1,6 +1,18 @@
 // Declaracion de funcion
-function saludar(){
-    console.log('Hola mundo')
+function saludar(nombre){
+    console.log('Hola ', arguments);
 }
 
-saludar()
+saludar('jesus');
+
+// Declaracion de funcion anonima
+const saludar2 = function(nombre){
+    console.log('Hola', nombre);
+}
+
+saludar2('juan');
+
+// Declaracion de funcion de flecha
+const saludarFlecha = (nombre) => console.log('Hola', nombre);
+
+saludarFlecha('Mundo');
