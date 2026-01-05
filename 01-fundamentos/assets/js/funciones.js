a7d3fee666ad6c53175f5ea23b79404d69918116
JesusAntonioSG12 <jesusantonio1506@gmail.com>
2026-01-05 17:29:57 -0600

    uso de return

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -1,18 +1,22 @@
 // Declaracion de funcion
 function saludar(nombre){
-    console.log('Hola ', arguments);
+    console.log('Hola ' + nombre);
+    return 1
 }
 
-saludar('jesus');
+console.log(saludar('jesus'));
 
 // Declaracion de funcion anonima
 const saludar2 = function(nombre){
-    console.log('Hola', nombre);
+    console.log('Hola' + nombre);
 }
 
 saludar2('juan');
 
 // Declaracion de funcion de flecha
-const saludarFlecha = (nombre) => console.log('Hola', nombre);
+const saludarFlecha = (nombre) => console.log('Hola' + nombre);
 
 saludarFlecha('Mundo');
+
+const getAleatorio = () => Math.random();
+console.log(getAleatorio())
