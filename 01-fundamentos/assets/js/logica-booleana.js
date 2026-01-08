a4322475463e85a03d2605a83f92fece279e684e
JesusAntonioSG12 <jesusantonio1506@gmail.com>
2026-01-08 10:55:05 -0600

    Update script references and add logical operators examples

---
+++ 01-fundamentos/assets/js/logica-booleana.js
@@ -0,0 +1,13 @@
+// operador not (!)
+console.log(!true); 
+console.log(!false);
+
+// operador and (&&)
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+
+// operador or (||)
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
